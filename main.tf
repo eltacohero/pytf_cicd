053d6bcbec58e477d825dab9cfe8fc8e4926a3fe
eltacohero <jrm.grondin@protonmail.com>
2020-10-17 03:53:57 -0400

    added export aws keys in pipeline

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = var.aws_region
-  profile = "default"
 }
 
 data "archive_file" "lambda_zip" {
--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "aws_region" {
   default = "eu-west-3"
 }
 
-variable "aws_profile" {
-  default = "default"
-}
-
 variable "runtime" {
   type    = string
   default = "python3.8"
