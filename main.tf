03e9d55800645cbf1f2b551dbc9950ea7e8177e4
eltacohero <jrm.grondin@protonmail.com>
2020-10-17 04:28:38 -0400

    rollback index.py main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_file = "https://gitlab.com/eltacohero/terraform/-/tree/dev/Python/index.py"
+  source_file = "../Python/index.py"
   output_path = "lambda_function.zip"
 }
 
